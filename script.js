024fd44549125b772025ead6a0653d532e08552f
Kundan Kumar Sahu <espkundan@gmail.com>
2023-02-09 13:06:16 +0530

    added post

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.use(bodyParser.urlencoded({extended: true}));
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=45807daff0e4cc1174d6e6bf69a97839&units=metric";
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body.cityName);
+  const query = req.body.cityName;
+  const apiKey = '45807daff0e4cc1174d6e6bf69a97839';
+  const unit = 'metric';
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`;
 
   https.get(url, (response) => {
     console.log('statusCode:', response.statusCode);
@@ -22,18 +34,70 @@ app.get('/', (req, res) => {
       // }
       // console.log(JSON.stringify(object));
 
-      const temperature = weatherData.weather[0].id;
+      const main = weatherData.weather[0].main;
+      const temperature = weatherData.main.temp;
 
-      console.log(temperature);
+      res.write(`<p>The Weather in ${query} is currently ${temperature} degree celcius.</p>`);
+
+      res.write(`<h1>The main is ${main}</h1>`);
+
+      const icon = weatherData.weather[0].icon;
+
+      const image = `<img src="http://openweathermap.org/img/wn/${icon}@4x.png">`;
+
+      res.write(image);
+
+      res.send();
+
+      console.log(main);
 
       // console.log(d.toString());
       // process.stdout.write(d);
     });
-
   });
+})
 
-  res.send('Hello, weather');
-});
+// const query = 'Ranchi';
+// const apiKey = '45807daff0e4cc1174d6e6bf69a97839';
+// const unit = 'metric';
+
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`;
+
+// https.get(url, (response) => {
+//   console.log('statusCode:', response.statusCode);
+//   console.log('headers:', response.headers);
+
+//   response.on('data', (d) => {
+//     const weatherData = JSON.parse(d);
+//     console.log(weatherData);
+
+//     // const object = {
+//     //   weather: 30,
+//     //   city: "Flash"
+//     // }
+//     // console.log(JSON.stringify(object));
+
+//     const main = weatherData.weather[0].main;
+//     const temperature = weatherData.main.temp;
+
+//     res.write(`<p>The Weather is currently ${temperature} degree celcius.</p>`);
+
+//     res.write(`<h1>The main is ${main}</h1>`);
+
+//     const icon = weatherData.weather[0].icon;
+
+//     const image = `<img src="http://openweathermap.org/img/wn/${icon}@4x.png">`;
+
+//     res.write(image);
+
+//     res.send();
+
+//     console.log(main);
+
+//     // console.log(d.toString());
+//     // process.stdout.write(d);
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
